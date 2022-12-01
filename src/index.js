e35a671a89d75ec6b0d8efcd664b63a8e3fd726f
YuriyRysak <yuriy.risak@gmail.com>
2022-12-01 17:53:14 +0200

    summary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const countryContainer = document.querySelector('country-info');
+input.addEventListener('input', event => {
+    event.preventDefault();
+    const nameCountry = event.target.value;
+    if( nameCountry === '') {
+        return alert('пустая строка');
+    }
+    fetchCountries(nameCountry).then(data => {
+        countryContainer.innerHTML = data.results.map(country => `<div>
+        <h2>${country.name}</h2>
+        <p>${country.name.official}</p>
+        <p>${population}</p>
+        <p>${flags.svg}</p>
+        <p>${languages}</p>
+        </div>`).join("");
+    });
+});
+
+
+function fetchCountries(nameCountry) {
+    
+    return fetch(`https://restcountries.com/v3.1/name/${nameCountry}?fields=,name,capital,population,flags,languages`
+    ).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    });
+
+} 
+fetchCountries('peru').then(data => console.log(data))
